dbe077d728a29c27bfae5eb6e63c6e767a816526
tvsraghuram <tvsraghuram@gmail.com>
2020-12-02 19:07:16 +0000

    Making some changes2

--- src/u/axone/developerintest/AppClass2.java
+++ src/u/axone/developerintest/AppClass2.java
@@ -2,6 +2,6 @@ package u.axone.developerintest;
 
 public class AppClass2 {
         public void doSomething2(){
-            System.out.println("do something");
+            System.out.println("do somethingggg");
         }}
 
